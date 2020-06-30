7fd043803c8059104c39c26d9553d5c78767c516
Source-Pallavi <59175852+Source-Pallavi@users.noreply.github.com>
2020-06-30 10:41:06 +0530

    Starting Mood Analyser

---
+++ MoodAnalyserTest/TEST.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserTEST.MoodAnalyserTest
+{
+    class TEST
+    {
+
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using 
+namespace MoodAnalyserTEST
+{
+    public class Tests
+    {
+        MoodAnalyser mood = new MoodAnalyser();
+        [SetUp]
+        public void Setup()
+        {
+            MoodAnalyser 
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
